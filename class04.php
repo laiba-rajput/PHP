f4e5a401c9015a97f5fb024eac54791b57e9d3eb
Laiba <laibaqueen754@gmail.com>
2024-11-25 22:40:12 +0500

    php homework

---
+++ class04.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //  $day = "Monday";
+        //  $day = "Friday";
+        //  switch ($day) {
+        //      case "Monday":
+        //          echo "Start of the work week!";
+        //          break;
+        //      case "Friday":
+        //          echo "Almost weekend!";
+        //          break;
+        //      default:
+        //          echo "Have a great day!";}
+
+        // $num = 10;
+        // $num_02 = 100;
+        //   echo($num < $num_02);
+
+
+        // $num  = 10;
+// $num_02 = "10";
+
+// airthmrtic operator
+// echo($num + $num_02);
+// echo($num - $num_02);
+// echo($num * $num_02);
+// echo($num / $num_02);
+// echo($num % $num_02);
+
+//assignment operator
+
+// $num =-5 ;
+// $num =+59 ;
+// echo($num);
+
+// comparison operator
+
+// echo ($num != $num_02);
+// echo ($num === $num_02);
+// echo ($num >= $num_02);
+//  echo ($num !== $num_02);
+
+// $admin = "Abdullah";
+// $pass = 12345;
+// $admin_02 = "Irfan";
+// $pass_02 = 123456;
+
+// if ($admin == "abdullah" && $pass == 12345 || $admin_02 == "Irfan" && $pass_02 == 123456) {
+//     echo ("Welcome Admin");
+// } else {
+//     echo ("Login Filed");
+// }
+
+// switch {
+//     case "";
+//     break;
+
+//     default;
+
+         
+
+         //  $colors=["red","blue","green","pink","orange","purple"];
+        //        echo $colors[3];
+
+        // $colors=["red","blue","green","pink","orange","purple"];
+        //       $colors[2] = "yellow";
+        //       echo $colors[2];   
+
+        // $colors=["red","blue","green","pink","orange","purple"];
+        // $countt = count($colors);
+        //    echo"$countt";
+
+        // $colors=["red","blue","green","pink","orange","purple"];
+        // unset($colors["0"]);
+        //   foreach($colors as $color) {
+        //     echo "<script>alert('$color')</script>";}
+
+
+        // $cars = array (
+        //     array("Volvo",22,18),
+        //     array("BMW",15,13),
+        //     array("Saab",5,2),
+        //     array("Land Rover",17,15),
+        //     array("carola",16,6),
+        //   );
+
+        // for ($row = 0; $row < 5; $row++) {
+        //     echo "<p><b>Row number $row</b></p>";
+        //     echo "<ul>";
+        //       for ($col = 0; $col < 3; $col++) {
+        //         echo "<li>".$cars[$row][$col]."</li>";
+        //       }
+        //     echo "</ul>";
+        //   }
+    ?>
+</body>
+</html>
