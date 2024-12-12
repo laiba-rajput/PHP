4f8a726e1ebd2ac31aa6da443406afe50bf1b113
Laiba <laibaqueen754@gmail.com>
2024-12-13 00:22:02 +0500

    php homework

---
+++ oop.php
@@ -0,0 +1,52 @@
+<?php
+  // class Human {
+  //   public $name;
+  //   public $age;
+
+  //   function set_age($age){
+  //       $this->age = $age;
+  //   }
+  //       function get_age(){
+  //           return $this->age;
+  //       }
+
+  //       function set_name($name){
+  //         $this->name = $name;
+  //     }
+  //         function get_name(){
+  //             return $this->name;
+  //         }
+  // }
+
+  // $human = new Human();
+  // $human->set_age(10);
+  // //echo $human->get_age();
+
+  // $human = new Human();
+  // $human->set_name("shoni");
+  // echo "hello my name is laibazoya and my cat name is" .$human->get_name(). "my cat age is" .$human->get_age();
+
+//     class fruit{
+//       public $name;
+//       public $color;
+//       function set_name($name){
+//         $this->name = $name;
+//     }
+//     function get_name(){
+//       return $this->name;
+//     }
+//     function set_color($color){
+//       $this->color = $color;
+//   }
+//   function get_color(){
+//     return $this->color;
+//   }
+// }
+//     $fruitname = new fruit();
+//     $fruitname->set_name("apple");
+//     echo "the healthiest fruit in the world is" .$fruitname->get_name();
+
+//     $fruitcolor = new fruit();
+//     $fruitcolor->set_color("red");
+//     echo "<br> the color of apple is mostly" .$fruitcolor->get_color();
+  ?>
