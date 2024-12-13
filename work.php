a0483de8b62c52c040dabae857147152fc02cc05
Laiba <laibaqueen754@gmail.com>
2024-12-14 03:43:27 +0500

    php homework

---
+++ work.php
@@ -0,0 +1,54 @@
+<?php
+
+//1-OOP - Inheritance
+// class Animal{
+//         public $name;
+//         public $age;
+//         public function __construct($name, $age) {
+//             $this->name = $name;
+//             $this->age = $age;
+//        }
+//         public function show($gender) {
+//             echo $this->name ."gender:". $gender;
+//        }
+//     }
+//          class Dog extends Animal{
+//             public function details(){
+//                 echo "my dog name is". $this->name ."and his age is". $this->age. "<br>";
+//             }
+//          } 
+         
+//          $animal = new Animal("foxy",4);
+//          $dog = new Dog("tommy",5);
+//          $dog->details();
+//          $animal->show("male");
+
+
+//2-OOP - Inheritance
+// class fruit{
+//     public $name;
+//     public $color;
+
+//     public function __construct($name, $color) {
+//         $this->name = $name;
+//         $this->color = $color;
+//     }
+//     public function show(){
+//         echo "Name: ". $this->name. ", color: ". $this->color. "<br>";
+//     }
+
+    
+// }
+
+// class healthiest extends fruit{
+//     public function details(){
+//         echo "this fruit name is " . $this->name . " this fruit color " . $this->color;
+//     }
+    
+// }
+
+// $fruit = new fruit("mango", "yellow");
+// $healthiest =new healthiest("apple" , "red");
+// $healthiest->details();
+
+?>
